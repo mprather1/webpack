58b42509a6d37277a9aa7839a5b35bc4ee62c69b
Michael Prather <mprather1@gmail.com>
2016-09-28 13:29:41 -0500

    works

--- app/index.js
+++ app/index.js
@@ -1,4 +1,36 @@
-var user = function(){
-  return "hello"
-}
-user()
+var Child = React.createClass({
+  displayName: "Child",
+
+  render: function () {
+    return React.createElement(
+      "div",
+      null,
+      "and this is the ",
+      React.createElement(
+        "b",
+        null,
+        this.props.name
+      ),
+      "."
+    );
+  }
+});
+
+var Parent = React.createClass({
+  displayName: "Parent",
+
+  render: function () {
+    return React.createElement(
+      "div",
+      null,
+      React.createElement(
+        "div",
+        null,
+        " This is the parent. "
+      ),
+      React.createElement(Child, { name: "child" })
+    );
+  }
+});
+
+ReactDOM.render(React.createElement(Parent, null), document.getElementById('app'));
